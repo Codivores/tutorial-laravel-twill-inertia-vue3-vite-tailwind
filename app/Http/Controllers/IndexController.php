2e99f80f14ef8b10c0f21a1ed50ea040de248fee
agnonym <contact@agnonym.com>
2021-07-14 16:49:18 +0200

    + | Controller / Route / Page - Index

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Index');
+    }
+}
---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Welcome</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  setup() {
+    console.log("Page - Index")
+  },
+})
+</script>
