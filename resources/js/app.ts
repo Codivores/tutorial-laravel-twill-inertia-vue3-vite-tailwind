b916fd24798e8993f31813e6b2f80bbd2654206c
agnonym <contact@agnonym.com>
2021-07-14 16:28:00 +0200

    + | JS - App

---
+++ resources/js/app.ts
@@ -0,0 +1,18 @@
+import { createApp, h } from "vue"
+import { App, plugin as inertiaPlugin } from "@inertiajs/inertia-vue3"
+import "vite/dynamic-import-polyfill"
+
+const el = document.getElementById("app")!
+
+createApp({
+  render: () =>
+    h(App, {
+      initialPage: JSON.parse(el.dataset.page!),
+      resolveComponent: async (name: string) => {
+        const page = (await import(`./Pages/${name}.vue`)).default
+        return page
+      },
+    }),
+})
+  .use(inertiaPlugin)
+  .mount(el)
