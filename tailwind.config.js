e78352eeaeb52cfc270b5a9dbb34e2a5c90f6174
agnonym <contact@agnonym.com>
2021-07-15 21:02:44 +0200

    § | Config - Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    purge: [
+        './resources/**/*.blade.php',
+        './resources/**/*.vue',
+    ],
+    darkMode: false,
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
