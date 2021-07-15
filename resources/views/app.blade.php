32bfb1b4ce4861ec9ca8781d3fbf652d9605ff18
agnonym <contact@agnonym.com>
2021-07-15 21:05:23 +0200

    * | View - App: add CSS for production build

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,7 @@
                 $manifest = json_decode(file_get_contents(public_path('dist/manifest.json')), true);
             @endphp
             <script type="module" src="/dist/{{ $manifest['resources/js/app.ts']['file'] }}"></script>
-            {{-- <link rel="stylesheet" href="/dist/{{ $manifest['resources/js/app.ts']['css'][0] }}"> --}}
+            <link rel="stylesheet" href="/dist/{{ $manifest['resources/js/app.ts']['css'][0] }}">
         @else
             <script type="module" src="http://localhost:3030/@vite/client"></script>
             <script type="module" src="http://localhost:3030/resources/js/app.ts"></script>
