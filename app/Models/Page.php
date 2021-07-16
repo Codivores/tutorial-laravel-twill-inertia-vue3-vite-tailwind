24335939ef539d2e005a1586062aab58378a281a
agnonym <contact@agnonym.com>
2021-07-16 15:45:20 +0200

    + | Module - Page

---
+++ app/Models/Page.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use A17\Twill\Models\Behaviors\HasBlocks;
+use A17\Twill\Models\Behaviors\HasTranslation;
+use A17\Twill\Models\Behaviors\HasSlug;
+use A17\Twill\Models\Behaviors\HasRevisions;
+use A17\Twill\Models\Behaviors\HasPosition;
+use A17\Twill\Models\Behaviors\Sortable;
+use A17\Twill\Models\Model;
+
+class Page extends Model implements Sortable
+{
+    use HasBlocks, HasTranslation, HasSlug, HasRevisions, HasPosition;
+
+    protected $fillable = [
+        'published',
+        'title',
+        'description',
+        'position',
+    ];
+    
+    public $translatedAttributes = [
+        'title',
+        'description',
+        'active',
+    ];
+    
+    public $slugAttributes = [
+        'title',
+    ];
+    
+}
