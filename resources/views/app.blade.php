91508bf3b01a5d527856c67a878ccb2bafd5d069
agnonym <contact@agnonym.com>
2023-04-14 14:32:17 +0200

    + | View - App

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/js/app.ts')
+        @inertiaHead
+    </head>
+    <body>
+        @inertia
+    </body>
+</html>
