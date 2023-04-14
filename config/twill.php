43711c7b1f0a60471d311b562c0d3f0d0bad880e
agnonym <contact@agnonym.com>
2023-04-14 17:27:15 +0200

    § | Config - Twill / Translatable

---
+++ config/twill.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Twill default tables naming configuration
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'blocks_table' => 'content_blocks',
+    'features_table' => 'content_features',
+    'fileables_table' => 'content_fileables',
+    'files_table' => 'content_files',
+    'mediables_table' => 'content_mediables',
+    'medias_table' => 'content_medias',
+    'password_resets_table' => 'admin_password_resets',
+    'related_table' => 'content_related',
+    'settings_table' => 'content_settings',
+    'tagged_table' => 'content_tagged',
+    'tags_table' => 'content_tags',
+    'users_oauth_table' => 'admin_users_oauth',
+    'users_table' => 'admin_users',
+
+];
