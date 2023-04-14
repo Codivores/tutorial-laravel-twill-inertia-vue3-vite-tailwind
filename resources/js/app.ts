93e38e2c5bd09ce7257112efa36b5f653f19b87d
agnonym <contact@agnonym.com>
2023-04-14 14:31:10 +0200

    + | JS - App: handle Inertia App

---
+++ resources/js/app.ts
@@ -0,0 +1,16 @@
+import { createApp, h, type DefineComponent } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+
+createInertiaApp({
+  resolve: async (name: string) => {
+    let page = await resolvePageComponent(`../views/Pages/${name}.vue`, import.meta.glob<DefineComponent>('../views/Pages/**/*.vue'))
+
+    return page
+  },
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el)
+  },
+})
