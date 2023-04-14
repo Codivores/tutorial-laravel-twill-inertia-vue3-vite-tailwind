08b09e4c9228f5ae6fe1d72ac0d75e47a7ee21d2
agnonym <contact@agnonym.com>
2023-04-14 16:39:33 +0200

    § | Config - Tailwind / PostCSS

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './resources/**/*.blade.php',
+    './resources/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
