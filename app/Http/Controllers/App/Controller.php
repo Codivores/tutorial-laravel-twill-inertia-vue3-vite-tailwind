610a6bc8c63037458fba836b3b8c2f50ddf7d639
agnonym <contact@agnonym.com>
2023-04-16 11:14:48 +0200

    * | Controller - App - Controller: change namespace to App\Http\Controllers\App

--- app/Http/Controllers/App/Controller.php
+++ app/Http/Controllers/App/Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\App;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
