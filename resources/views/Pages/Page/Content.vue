4a89a4449f7de7a48617d4ff87443c0c8242a42e
agnonym <contact@agnonym.com>
2023-04-17 12:33:40 +0200

    * | Page - App - Page Content: add Head component

--- resources/views/Pages/Page/Content.vue
+++ resources/views/Pages/Page/Content.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import Head from '@Theme/Head.vue'
+
 defineOptions({
   name: 'PageContent',
   layoutName: 'Default',
@@ -12,6 +14,7 @@ defineProps<Props>()
 </script>
 
 <template>
+  <Head :item="item"></Head>
   <h1 class="text-center text-4xl font-semibold text-gray-900">
     {{ item.title }}
   </h1>
