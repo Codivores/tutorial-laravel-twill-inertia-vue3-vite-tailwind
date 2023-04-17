5b5ad4c6b354603beca2bc91053a2afcaa0053ff
agnonym <contact@agnonym.com>
2023-04-17 12:33:10 +0200

    + | Theme - Component - Head

---
+++ resources/views/Components/Theme/Head.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { Head as InertiaHead } from '@inertiajs/vue3'
+
+defineOptions({
+  name: 'ThemeHead',
+})
+
+interface Props {
+  item?: any
+  title?: string
+  description?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  item: () => ({}),
+  title: '',
+  description: '',
+})
+
+const appName = 'My Application'
+
+const title =
+  props.item && props.item.meta_title && props.item.meta_title != ''
+    ? props.item.meta_title
+    : props.item && props.item.title && props.item.title != ''
+    ? props.item.title
+    : props.title
+
+const description =
+  props.item && props.item.meta_description && props.item.meta_description != ''
+    ? props.item.meta_description
+    : props.item && props.item.description && props.item.description != ''
+    ? props.item.description
+    : props.description
+</script>
+
+<template>
+  <InertiaHead :title="title ? `${title} | ${appName}` : appName">
+    <template v-if="description !== ''">
+      <meta
+        head-key="description"
+        name="description"
+        :content="description"
+      />
+    </template>
+  </InertiaHead>
+</template>
