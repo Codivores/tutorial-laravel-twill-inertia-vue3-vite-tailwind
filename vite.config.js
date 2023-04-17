ea0d1c1d5a304d1ea6eba2ddb4f70b2112dfa069
agnonym <contact@agnonym.com>
2023-04-17 11:25:58 +0200

    § | Config - Vite: add DefineOptions plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import DefineOptions from 'unplugin-vue-define-options/vite';
 
 export default defineConfig({
     plugins: [
@@ -26,5 +27,6 @@ export default defineConfig({
                 },
             },
         }),
+        DefineOptions(),
     ],
 });
