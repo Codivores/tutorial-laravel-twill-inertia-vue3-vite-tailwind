3ad2e2bd3b6ce2ef4db9229afb4dee634d5d8af2
agnonym <contact@agnonym.com>
2023-04-17 11:27:09 +0200

    + | Layout - Component - Default / FullPage

---
+++ resources/views/Components/Layout/Default.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'LayoutDefault',
+})
+</script>
+
+<template>
+  <div class="min-h-full">
+    <header class="bg-white shadow">
+      <div class="mx-auto max-w-7xl px-4 py-6">
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">My Application</h1>
+      </div>
+    </header>
+    <main>
+      <div class="mx-auto max-w-7xl py-6">
+        <slot></slot>
+      </div>
+    </main>
+  </div>
+</template>
---
+++ resources/views/Components/Layout/FullPage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'LayoutFullPage',
+})
+</script>
+
+<template>
+  <main class="w-full flex flex-col h-screen content-center justify-center">
+    <slot></slot>
+  </main>
+</template>
