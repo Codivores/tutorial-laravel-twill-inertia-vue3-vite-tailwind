d37535661decd8f1e38c33acb1092cf870c30ed3
agnonym <contact@agnonym.com>
2023-06-01 11:17:36 +0200

    §> | Package: vue 3.3.4 / vite 4.3.9 / vite plugin-vue 4.2.3 / inertia 1.0.7 / laravel-vite-plugin 0.7.8 / tailwind 3.3.2 / postcss 8.4.24 / remove unplugin-vue-define-options (included in vue 3.3)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import DefineOptions from 'unplugin-vue-define-options/vite';
 
 export default defineConfig({
     resolve: {
@@ -43,6 +42,5 @@ export default defineConfig({
                 },
             },
         }),
-        DefineOptions(),
     ],
 });
