ad09e45636439f20500c35750e77c0d82abfbaf9
agnonym <contact@agnonym.com>
2023-12-19 22:19:02 +0100

    § | Config - Vite / TypeScript: add @Block alias

--- vite.config.js
+++ vite.config.js
@@ -1,58 +1,61 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
-import AutoImport from 'unplugin-auto-import/vite';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
-    resolve: {
-        alias: [
-          {
-            find: '@Composable',
-            replacement: '/resources/js/Composables'
-          },
-          {
-            find: "@Form",
-            replacement: `/resources/views/Components/Form`,
-          },
-          {
-            find: '@Theme',
-            replacement: '/resources/views/Components/Theme'
-          }
-        ]
-    },
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.ts'],
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    // The Vue plugin will re-write asset URLs, when referenced
-                    // in Single File Components, to point to the Laravel web
-                    // server. Setting this to `null` allows the Laravel plugin
-                    // to instead re-write asset URLs to point to the Vite
-                    // server instead.
-                    base: null,
- 
-                    // The Vue plugin will parse absolute URLs and treat them
-                    // as absolute paths to files on disk. Setting this to
-                    // `false` will leave absolute URLs un-touched so they can
-                    // reference assets in the public directory as expected.
-                    includeAbsolute: false,
-                },
-            },
-        }),
-        AutoImport({
-            // targets to transform
-            include: [
-                /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
-                /\.vue$/, /\.vue\?vue/, // .vue
-                /\.md$/, // .md
-            ],
-            imports: [
-                'vue',
-            ],
-        }),
+  resolve: {
+    alias: [
+      {
+        find: '@Block',
+        replacement: '/resources/views/Components/Theme/Block',
+      },
+      {
+        find: '@Composable',
+        replacement: '/resources/js/Composables',
+      },
+      {
+        find: '@Form',
+        replacement: `/resources/views/Components/Form`,
+      },
+      {
+        find: '@Theme',
+        replacement: '/resources/views/Components/Theme',
+      },
     ],
-});
+  },
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.ts'],
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          // The Vue plugin will re-write asset URLs, when referenced
+          // in Single File Components, to point to the Laravel web
+          // server. Setting this to `null` allows the Laravel plugin
+          // to instead re-write asset URLs to point to the Vite
+          // server instead.
+          base: null,
+
+          // The Vue plugin will parse absolute URLs and treat them
+          // as absolute paths to files on disk. Setting this to
+          // `false` will leave absolute URLs un-touched so they can
+          // reference assets in the public directory as expected.
+          includeAbsolute: false,
+        },
+      },
+    }),
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+      imports: ['vue'],
+    }),
+  ],
+})
