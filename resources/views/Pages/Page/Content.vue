61cd6302eb1e26a302d6f478e7cd08b47184d323
agnonym <contact@agnonym.com>
2023-12-20 15:02:01 +0100

    * | Page - App - Page Content: handle CommonTitle Block

--- resources/views/Pages/Page/Content.vue
+++ resources/views/Pages/Page/Content.vue
@@ -1,21 +1,30 @@
 <script setup lang="ts">
 import Head from '@Theme/Head.vue'
 
-defineOptions({
-  name: 'PageContent',
-  layoutName: 'Default',
-})
-
 interface Props {
   item: Model.Page
 }
 
 defineProps<Props>()
+
+const BlockCommonTitle = defineAsyncComponent(() => import('@Block/Common/Title.vue'))
 </script>
 
 <template>
   <Head :item="item"></Head>
-  <h1 class="text-center text-4xl font-semibold text-gray-900">
-    {{ item.title }}
-  </h1>
+
+  <div
+    v-if="item?.blocks && Array.isArray(item.blocks) && item.blocks.length > 0"
+    class="mx-auto w-full max-w-6xl"
+  >
+    <div
+      v-for="(block, index) in item.blocks"
+      :key="index"
+    >
+      <BlockCommonTitle
+        v-if="block.type == 'common-title'"
+        :block="block"
+      ></BlockCommonTitle>
+    </div>
+  </div>
 </template>
